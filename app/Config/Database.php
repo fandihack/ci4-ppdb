793bb20a416fd2a4a18bfb0fcb4b74daef3f2853
fannndi <trinity.x.dev@gmail.com>
2026-01-16 19:29:28 +0700

    10

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -4,36 +4,21 @@ namespace Config;
 
 use CodeIgniter\Database\Config;
 
-/**
- * Database Configuration
- */
 class Database extends Config
 {
-    /**
-     * The directory that holds the Migrations and Seeds directories.
-     */
     public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
-
-    /**
-     * Lets you choose which connection group to use if no other is specified.
-     */
     public string $defaultGroup = 'default';
 
-    /**
-     * The default database connection.
-     *
-     * @var array<string, mixed>
-     */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => env('MYSQLHOST', 'db'), 
-        'username'     => env('MYSQLUSER', 'ci4'), 
-        'password'     => env('MYSQLPASSWORD', 'ci4'), 
-        'database'     => env('MYSQLDATABASE', 'ci4db'), 
+        'hostname'     => 'localhost',
+        'username'     => 'root',
+        'password'     => '',
+        'database'     => 'ci4',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
-        'DBDebug'      => (ENVIRONMENT !== 'production'),
+        'DBDebug'      => true,
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
@@ -41,7 +26,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => (int) env('MYSQLPORT', 3306),
+        'port'         => 3306,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
@@ -51,12 +36,6 @@ class Database extends Config
         ],
     ];
 
-    /**
-     * This database connection is used when
-     * running PHPUnit database tests.
-     *
-     * @var array<string, mixed>
-     */
     public array $tests = [
         'DSN'         => '',
         'hostname'    => '127.0.0.1',
@@ -64,7 +43,7 @@ class Database extends Config
         'password'    => '',
         'database'    => ':memory:',
         'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. CI3 will prefix existing tables and fails
+        'DBPrefix'    => 'db_',
         'pConnect'    => false,
         'DBDebug'     => true,
         'charset'     => 'utf8',
@@ -83,9 +62,13 @@ class Database extends Config
     {
         parent::__construct();
 
-        // Ensure that we always set the database group to 'tests' if
-        // we are currently running an automated test suite, so that
-        // we don't overwrite live data on accident.
+        // Mengisi data database secara dinamis dari Environment Variables Railway
+        $this->default['hostname'] = env('MYSQLHOST', 'db');
+        $this->default['username'] = env('MYSQLUSER', 'ci4');
+        $this->default['password'] = env('MYSQLPASSWORD', 'ci4');
+        $this->default['database'] = env('MYSQLDATABASE', 'ci4db');
+        $this->default['port']     = (int) env('MYSQLPORT', 3306);
+
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
         }
