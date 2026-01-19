20b5f7bd4bcd49c3033b77ece2f3af73b30f8397
fannndi <trinity.x.dev@gmail.com>
2026-01-19 12:37:13 +0700

    28

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -10,12 +10,14 @@ class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
+        // Gunakan helper base_url() untuk memastikan URL absolut sesuai domain Railway
         if (! session()->get('admin_logged_in')) {
-            return redirect()->to('/admin');
+            return redirect()->to(base_url('admin'));
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
+        // Kosong
     }
 }
